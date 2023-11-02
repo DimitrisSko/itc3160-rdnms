ff88e8f8486230e798f8a73304f695508deff4d5
YannisPatsios <149704508+YannisPatsios@users.noreply.github.com>
2023-11-02 20:30:25 +0200

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,2 +1,8 @@
---4
-Select w.LASTNAME || ' ' || w.FIRSTNAME AS "FULLNAME" FROM WORKER w, PROJECT p, ASSIGN a WHERE p.PROJNO = a.PROJNO AND a.EMPID = w. EMPID AND p.PROJNO = 1040 ORDER BY w.LASTNAME;
+--4. Display an alphabetical list of names of all workers assigned to project 1040 (‘Bruno’), sorted by last name. (10points)
+Select W.LASTNAME || ' ' || W.FIRSTNAME AS "FULLNAME", P.PROJNO 
+    FROM WORKER W, PROJECT P, ASSIGN A 
+    WHERE P.PROJNO = A.PROJNO 
+    AND A.EMPID = W. EMPID 
+    AND P.PROJNO = 1040 
+    ORDER BY W.LASTNAME
+    ;
