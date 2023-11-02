a2bd8a5a4bc5d4b539fb7709d4d811e9d50d3e39
YannisPatsios <149704508+YannisPatsios@users.noreply.github.com>
2023-11-02 20:51:27 +0200

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -1,4 +1,4 @@
---1
+--1. Display the full names of all workers in the Development department whose salaries are above 30.000. Use the ‘Development’ as a condition and not the department id. (10points)
 Select w.LASTNAME || ' ' || w.FIRSTNAME AS "FULLNAME" 
   FROM WORKER w, DEPT d 
   WHERE w.DEPARTMENTID = d.DEPARTMENTID 
