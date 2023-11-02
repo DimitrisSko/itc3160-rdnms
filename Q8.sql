b58c1e69f2ad3a0f3c05b469195a8bc8b63488af
YannisPatsios <149704508+YannisPatsios@users.noreply.github.com>
2023-11-02 20:07:58 +0200

    Update Q8.sql

--- Q8.sql
+++ Q8.sql
@@ -1,2 +1,9 @@
---8
-Select w.LASTNAME || ' ' || w.FIRSTNAME AS "FULLNAME", a.RATING FROM WORKER w, PROJECT p, ASSIGN a, DEPT d WHERE w.LASTNAME = 'Liu' AND W.FIRSTNAME = 'Jun-Min' AND p.PROJNO = a.PROJNO AND a.EMPID = w. EMPID ORDER BY w.LASTNAME;
+--8. Display an alphabetical list of names and corresponding ratings of all workers on any project that is managed by Liu Jun-Min. Use ‘Liu’ and ‘Jun-Min’ as conditions. (10points)
+SELECT W.LASTNAME || ' ' || W.FIRSTNAME AS "FULLNAME", a.rating
+    FROM WORKER W
+    JOIN ASSIGN  A ON W.EMPID = A.EMPID
+    JOIN PROJECT P ON A.PROJNO = P.PROJNO
+    JOIN WORKER  WRKR ON P.PROJMGRID = WRKR.EMPID
+    WHERE WRKR.FIRSTNAME = 'Jun-Min'
+    AND WRKR.LASTNAME = 'Liu'
+    ;
