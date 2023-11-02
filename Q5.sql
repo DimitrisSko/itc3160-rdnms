2eb5e766e4e2246df7bdd2581cd7e9011a7490f5
YannisPatsios <149704508+YannisPatsios@users.noreply.github.com>
2023-11-02 20:31:25 +0200

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -1,2 +1,5 @@
---5
-Select w.LASTNAME || ' ' || w.FIRSTNAME AS "FULLNAME", w.SALARY FROM WORKER w, DEPT d WHERE w.DEPARTMENTID = d.DEPARTMENTID and w.SALARY > (SELECT AVG(SALARY) FROM WORKER) and d.DEPARTMENTNAME = 'Development' ;
+--5. Display the full name of all employees in the Development department with an above average salary. (10points)
+Select W.LASTNAME || ' ' || W.FIRSTNAME AS "FULLNAME", W.SALARY 
+    FROM WORKER W, DEPT D 
+    WHERE W.DEPARTMENTID = D.DEPARTMENTID AND W.SALARY > (SELECT AVG(SALARY) FROM WORKER) AND d.DEPARTMENTNAME = 'Development' 
+    ;
